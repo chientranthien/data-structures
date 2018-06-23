e94b43ba54f4ce5f93365714503edc536669f929
chien tran thien <vinadonquixote@gmail.com>
2018-06-23 09:06:37 +0700

    implemented add and get func

--- buffer_ring/src/main/java/com/chientt/bufferring/BufferRing.java
+++ buffer_ring/src/main/java/com/chientt/bufferring/BufferRing.java
@@ -1,21 +1,33 @@
 package com.chientt.bufferring;
 
 /**
- *
  * @author chientt
  */
 public class BufferRing<T> {
 
-    T[] data;
-    int pos;
-    int capacity;
+    Object[] data;
+    private int writePos;
+    private int readPos;
+    private int capacity;
 
     public BufferRing(int capacity) {
         this.capacity = capacity;
-        data = new T[capacity];
+        data = new Object[capacity];
     }
 
     public void add(T t) {
-        data[pos] = t;
+        data[writePos] = t;
+        writePos++;
+        if (writePos >= capacity)
+            writePos = 0;
+    }
+
+    public T get() {
+        T result = (T) data[readPos];
+        readPos++;
+        if (readPos >= capacity)
+            readPos = 0;
+
+        return result;
     }
 }
