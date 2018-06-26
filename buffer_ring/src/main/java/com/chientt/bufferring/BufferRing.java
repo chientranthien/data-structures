294cf90be92dbcea493e29c7a4c147f31110d2c4
chien tran thien <vinadonquixote@gmail.com>
2018-06-26 21:40:39 +0700

    coded avaiable func

--- buffer_ring/src/main/java/com/chientt/bufferring/BufferRing.java
+++ buffer_ring/src/main/java/com/chientt/bufferring/BufferRing.java
@@ -5,24 +5,45 @@ package com.chientt.bufferring;
  */
 public class BufferRing<T> {
 
-    Object[] data;
+    private Object[] data;
     private int writePos;
     private int readPos;
     private int capacity;
+    private boolean flipped;
 
     public BufferRing(int capacity) {
         this.capacity = capacity;
         data = new Object[capacity];
     }
 
-    public void add(T t) {
+    public void reset() {
+        this.writePos = 0;
+        this.readPos = 0;
+        this.flipped = false;
+    }
+
+    public int available() {
+        if (!flipped) {
+            return writePos - readPos;
+        }
+        return capacity - readPos + writePos;
+    }
+
+
+    public int remainingCapacity() {
+        if(!flipped){
+            return capacity - writePos;
+        }
+        return readPos - writePos;
+    }
+    public void put(T t) {
         data[writePos] = t;
         writePos++;
         if (writePos >= capacity)
             writePos = 0;
     }
 
-    public T get() {
+    public T take() {
         T result = (T) data[readPos];
         readPos++;
         if (readPos >= capacity)
