1328c5b4667e55334c687ef86939581b48d26960
Chien <1112026@student.hcmus.edu.vn>
2018-08-15 10:08:22 +0700

    implmented Node

--- b_tree/src/main/java/com/chientt/btree/BTree.java
+++ b_tree/src/main/java/com/chientt/btree/BTree.java
@@ -5,6 +5,9 @@ public class BTree {
     public static class Node {
 
         private int data;
+        private int[] keys;
         private Node[] childNodes;
+        private boolean leaf;
+        private int minimumDegree;
     }
 }
